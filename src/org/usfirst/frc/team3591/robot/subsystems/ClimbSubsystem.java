fa13b8accf20f41dc23c8550edd555a4039c2850
sharkMAH <maxdavis001@gmail.com>
2017-02-18 10:20:41 -0500

    fixxed mistake i found

--- src/org/usfirst/frc/team3591/robot/subsystems/ClimbSubsystem.java
+++ src/org/usfirst/frc/team3591/robot/subsystems/ClimbSubsystem.java
@@ -4,6 +4,8 @@ import org.usfirst.frc.team3591.robot.RobotMap;
 
 import com.ctre.CANTalon;
 
+import edu.wpi.first.wpilibj.JoystickBase;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -13,21 +15,25 @@ public class ClimbSubsystem extends Subsystem {
 
 	public CANTalon lcTalon;
 	public CANTalon rcTalon;
-	
+	public JoystickButton trigger;
+	public JoystickButton button;
+	public JoystickBase Joystick;
 	// Put methods for controlling this subsystem
     // here. Call these from Commands.
 
 public ClimbSubsystem(){
 	lcTalon = new CANTalon(RobotMap.LEFTCLIMBTALON_ID);
 	rcTalon = new CANTalon(RobotMap.RIGHTCLIMBTALON_ID);
+	trigger = new JoystickButton(Joystick, RobotMap.TRIGGER_ID);
+	button = new JoystickButton(Joystick, RobotMap.BUTTON_ID);
 }
 
-	public void ClimbTalon(){
+	public void trigger(){
 		lcTalon.set(1);
 		rcTalon.set(-1);
 	}
 
-	public void ClimbTalonDown(){
+	public void button(){
 		lcTalon.set(-1);
 		rcTalon.set(1);
 	}
